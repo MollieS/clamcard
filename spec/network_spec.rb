761457d1725dbfd8f3631bd6b5e1e1e1c9b25b66
Mollie Stephenson <mollie.c.stephenson@gmail.com>
2015-10-28 10:24:56 +0000

    adds coveralls

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -8,14 +8,20 @@ describe Network do
   it { is_expected.to respond_to(:touch).with(3).arguments }
 
   context 'calculating the cost of a journey' do
-    it 'should have a list of stations' do
-      expect(network.stations).to eq ({ zone_one: %w(Asterick Antelope Aldgate Angel Aardvark),
-                                        zone_two: %w(Bison Bugel Balham Bullhead Barbican) })
+    it 'should have a list of stations in zone_one' do
+      expect(network.stations[:zone_one]).to eq %w(Asterick Antelope Aldgate Angel Aardvark)
     end
 
-    it 'should know the prices' do
-      expect(network.prices).to eq ({ zone_one: 2.50,
-                                      zone_two: 3.00 })
+    it 'should have a list of stations in zone_two' do
+      expect(network.stations[:zone_two]).to eq %w(Bison Bugel Balham Bullhead Barbican)
+    end
+
+    it 'should know the price for a journey to zone_one' do
+      expect(network.prices[:zone_one]).to eq 2.50
+    end
+
+    it 'should know the price for a journey to zone_two' do
+      expect(network.prices[:zone_two]).to eq 3.00
     end
 
     it 'calculates the correct price for a zone_one journey' do
